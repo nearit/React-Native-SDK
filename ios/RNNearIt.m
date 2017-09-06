61dce9a39f1c17a8b23431f1d387c118b7be2731
Mattia Panzeri <mattia.panzeri93@gmail.com>
2017-09-06 17:57:34 +0200

    feat(iOS): Add `NearITSDK` dependency loading
    Module users should add `Podfile` under his application `ios` folder
    (using `pod init`) and add `pod 'NearITSDK'` as dependency)

--- ios/RNNearIt.m
+++ ios/RNNearIt.m
@@ -1,10 +1,24 @@
 
 #import "RNNearIt.h"
+#import <NearITSDK/NearITSDK.h>
 
 NSString* const RN_NATIVE_EVENTS_TOPIC = @"RNNearItEvent";
 
 NSString* const EVENT_TYPE_SIMPLE = @"NearIt.Events.SimpleNotification";
 NSString* const EVENT_TYPE_CUSTOM_JSON = @"NearIt.Events.CustomJSON";
+
+// Events content
+NSString* const EVENT_TYPE = @"type";
+NSString* const EVENT_TRACKING_INFO = @"trackingInfo";
+NSString* const EVENT_CONTENT = @"content";
+NSString* const EVENT_CONTENT_MESSAGE = @"message";
+NSString* const EVENT_CONTENT_DATA = @"data";
+NSString* const EVENT_FROM_USER_ACTION = @"fromUserAction";
+
+
+// Recipe Statuses
+NSString* const RECIPE_STATUS_NOTIFIED = @"RECIPE_STATUS_NOTIFIED";
+NSString* const RECIPE_STATUS_ENGAGED = @"RECIPE_STATUS_ENGAGED";
 
 @implementation RNNearIt
 
@@ -22,8 +36,17 @@ RCT_EXPORT_MODULE()
                         @"SimpleNotification": EVENT_TYPE_SIMPLE,
                         @"CustomJson": EVENT_TYPE_CUSTOM_JSON
                      },
-             @"EventContent": @{},
-             @"Statuses": @{}
+             @"EventContent": @{
+                        EVENT_TYPE: EVENT_TYPE,
+                        EVENT_TRACKING_INFO: EVENT_TRACKING_INFO,
+                        EVENT_CONTENT: EVENT_CONTENT,
+                        EVENT_CONTENT_MESSAGE: EVENT_CONTENT_MESSAGE,
+                        EVENT_CONTENT_DATA: EVENT_CONTENT_DATA
+                     },
+             @"Statuses": @{
+                        RECIPE_STATUS_NOTIFIED: NITRecipeNotified,
+                        RECIPE_STATUS_ENGAGED: NITRecipeEngaged
+                     }
             };
 }
 
