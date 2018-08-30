b8b819f4be41ead04d734fcf2adb5a16a2cf7bcc
Federico Boschini <federico@nearit.com>
2018-08-30 17:14:48 +0200

    chore(iOS): add contributor

--- ios/RNNearIt.m
+++ ios/RNNearIt.m
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017 Mattia Panzeri <mattia.panzeri93@gmail.com>
+ * Latest changes by Federico Boschini <federico@nearit.com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
