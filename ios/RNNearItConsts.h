a8a1bd8f05ee3e0266fea94391fd27a786121607
Federico Boschini <federico@nearit.com>
2019-06-11 17:04:58 +0200

    feat(ios): add consts file

---
+++ ios/RNNearItConsts.h
@@ -0,0 +1,17 @@
+//
+//  RNNearItConsts.h
+//  RNNearIt
+//
+//  Created by Federico Boschini on 11/06/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+@interface RNNearItConsts : NSObject
+
+extern NSString* EVENT_TYPE_SIMPLE;
+extern NSString* EVENT_TYPE_CUSTOM_JSON;
+extern NSString* EVENT_TYPE_COUPON;
+extern NSString* EVENT_TYPE_CONTENT;
+extern NSString* EVENT_TYPE_FEEDBACK;
+
+@end
---
+++ ios/RNNearItConsts.m
@@ -0,0 +1,19 @@
+//
+//  RNNearItConsts.m
+//  RNNearIt
+//
+//  Created by Federico Boschini on 11/06/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "RNNearItConsts.h"
+
+@implementation RNNearItConsts
+
+NSString* EVENT_TYPE_SIMPLE = @"NearIt.Events.SimpleNotification";
+NSString* EVENT_TYPE_CUSTOM_JSON = @"NearIt.Events.CustomJSON";
+NSString* EVENT_TYPE_COUPON = @"NearIt.Events.Coupon";
+NSString* EVENT_TYPE_CONTENT = @"NearIt.Events.Content";
+NSString* EVENT_TYPE_FEEDBACK = @"NearIt.Events.Feedback";
+
+@end
