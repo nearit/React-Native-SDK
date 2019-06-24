b43bebe94cf8a950bb7f2e4d0d54e36b17a508ab
Federico Boschini <federico@nearit.com>
2019-06-24 10:40:51 +0200

    feat(ios): consts file

--- ios/RNNearItConsts.h
+++ ios/RNNearItConsts.h
@@ -10,10 +10,76 @@
 
 @interface RNNearItConsts : NSObject
 
-extern NSString* EVENT_TYPE_SIMPLE;
-extern NSString* EVENT_TYPE_CUSTOM_JSON;
-extern NSString* EVENT_TYPE_COUPON;
-extern NSString* EVENT_TYPE_CONTENT;
-extern NSString* EVENT_TYPE_FEEDBACK;
+// Event topic
+extern const NSString* RN_NATIVE_EVENTS_TOPIC;
+extern const NSString* RN_NATIVE_NOTIFICATION_HISTORY_TOPIC;
+
+// Event type
+extern const NSString* EVENT_TYPE_SIMPLE;
+extern const NSString* EVENT_TYPE_CUSTOM_JSON;
+extern const NSString* EVENT_TYPE_COUPON;
+extern const NSString* EVENT_TYPE_CONTENT;
+extern const NSString* EVENT_TYPE_FEEDBACK;
+
+// Event content
+extern const NSString* EVENT_TYPE;
+extern const NSString* EVENT_TRACKING_INFO;
+extern const NSString* EVENT_CONTENT_MESSAGE;
+
+extern const NSString* EVENT_CONTENT;
+
+extern const NSString* EVENT_FROM_USER_ACTION;
+extern const NSString* EVENT_STATUS;
+
+// Common
+extern const NSString* EVENT_CONTENT_TITLE;
+// Image
+extern const NSString* EVENT_IMAGE;
+extern const NSString* EVENT_IMAGE_FULL_SIZE;
+extern const NSString* EVENT_IMAGE_SQUARE_SIZE;
+
+// Content
+extern const NSString* EVENT_CONTENT_TEXT;
+extern const NSString* EVENT_CONTENT_CTA;
+extern const NSString* EVENT_CONTENT_CTA_LABEL;
+extern const NSString* EVENT_CONTENT_CTA_URL;
+
+// Coupon
+extern const NSString* EVENT_COUPON_DESCRIPTION;
+extern const NSString* EVENT_COUPON_VALUE;
+extern const NSString* EVENT_COUPON_EXPIRES_AT;
+extern const NSString* EVENT_COUPON_REDEEMABLE_FROM;
+extern const NSString* EVENT_COUPON_SERIAL;
+extern const NSString* EVENT_COUPON_CLAIMED_AT;
+extern const NSString* EVENT_COUPON_REDEEMED_AT;
+
+// Feedback
+extern const NSString* EVENT_FEEDBACK_QUESTION;
+extern const NSString* EVENT_FEEDBACK_ID;
+
+// Notification History
+extern const NSString* NOTIFICATION_HISTORY_READ;
+extern const NSString* NOTIFICATION_HISTORY_TIMESTAMP;
+extern const NSString* NOTIFICATION_HISTORY_IS_NEW;
+extern const NSString* NOTIFICATION_HISTORY_CONTENT;
+
+// Permissions
+extern const NSString* PERMISSIONS_LOCATION_PERMISSION;
+extern const NSString* PERMISSIONS_NOTIFICATIONS_PERMISSION;
+extern const NSString* PERMISSIONS_BLUETOOTH;
+extern const NSString* PERMISSIONS_LOCATION_SERVICES;
+extern const NSString* PERMISSIONS_LOCATION_ALWAYS;
+extern const NSString* PERMISSIONS_LOCATION_DENIED;
+extern const NSString* PERMISSIONS_LOCATION_WHEN_IN_USE;
+
+// Error codes
+extern const NSString* E_SEND_FEEDBACK_ERROR;
+extern const NSString* E_PROFILE_ID_GET_ERROR;
+extern const NSString* E_PROFILE_ID_RESET_ERROR;
+extern const NSString* E_PROFILE_GET_USER_DATA_ERROR;
+extern const NSString* E_COUPONS_PARSING_ERROR;
+extern const NSString* E_COUPONS_RETRIEVAL_ERROR;
+extern const NSString* E_NOTIFICATION_HISTORY_RETRIEVAL_ERROR;
+extern const NSString* E_OPT_OUT_ERROR;
 
 @end
--- ios/RNNearItConsts.m
+++ ios/RNNearItConsts.m
@@ -10,10 +10,77 @@
 
 @implementation RNNearItConsts
 
+// Event topic
+NSString* RN_NATIVE_EVENTS_TOPIC = @"RNNearItEvent";
+NSString* RN_NATIVE_NOTIFICATION_HISTORY_TOPIC = @"RNNearItNotificationHistory";
+
+// Event type
 NSString* EVENT_TYPE_SIMPLE = @"NearIt.Events.SimpleNotification";
 NSString* EVENT_TYPE_CUSTOM_JSON = @"NearIt.Events.CustomJSON";
 NSString* EVENT_TYPE_COUPON = @"NearIt.Events.Coupon";
 NSString* EVENT_TYPE_CONTENT = @"NearIt.Events.Content";
 NSString* EVENT_TYPE_FEEDBACK = @"NearIt.Events.Feedback";
+
+// Event content
+NSString* EVENT_TYPE = @"type";
+NSString* EVENT_TRACKING_INFO = @"trackingInfo";
+NSString* EVENT_CONTENT_MESSAGE = @"message";
+
+NSString* EVENT_CONTENT = @"content";
+
+NSString* EVENT_FROM_USER_ACTION = @"fromUserAction";
+NSString* EVENT_STATUS = @"status";
+
+// Common
+NSString* EVENT_CONTENT_TITLE = @"title";
+// Image
+NSString* EVENT_IMAGE = @"image";
+NSString* EVENT_IMAGE_FULL_SIZE = @"fullSize";
+NSString* EVENT_IMAGE_SQUARE_SIZE = @"squareSize";
+
+// Content
+NSString* EVENT_CONTENT_TEXT = @"text";
+NSString* EVENT_CONTENT_CTA = @"cta";
+NSString* EVENT_CONTENT_CTA_LABEL = @"label";
+NSString* EVENT_CONTENT_CTA_URL = @"url";
+
+// Coupon
+NSString* EVENT_COUPON_DESCRIPTION = @"description";
+NSString* EVENT_COUPON_VALUE = @"value";
+NSString* EVENT_COUPON_EXPIRES_AT = @"expiresAt";
+NSString* EVENT_COUPON_REDEEMABLE_FROM = @"redeemableFrom";
+NSString* EVENT_COUPON_SERIAL = @"serial";
+NSString* EVENT_COUPON_CLAIMED_AT = @"claimedAt";
+NSString* EVENT_COUPON_REDEEMED_AT = @"reddemedAt";
+
+// Feedback
+NSString* EVENT_FEEDBACK_QUESTION = @"feedbackQuestion";
+NSString* EVENT_FEEDBACK_ID = @"feedbackId";
+
+// Notification History
+NSString* NOTIFICATION_HISTORY = @"notificationHistory";
+NSString* NOTIFICATION_HISTORY_READ = @"read";
+NSString* NOTIFICATION_HISTORY_TIMESTAMP = @"timestamp";
+NSString* NOTIFICATION_HISTORY_IS_NEW = @"isNew";
+NSString* NOTIFICATION_HISTORY_CONTENT = @"notificationContent";
+
+// Permissions
+NSString* PERMISSIONS_LOCATION_PERMISSION = @"location";
+NSString* PERMISSIONS_NOTIFICATIONS_PERMISSION = @"notifications";
+NSString* PERMISSIONS_BLUETOOTH = @"bluetooth";
+NSString* PERMISSIONS_LOCATION_SERVICES = @"locationServices";
+NSString* PERMISSIONS_LOCATION_ALWAYS = @"always";
+NSString* PERMISSIONS_LOCATION_DENIED = @"denied";
+NSString* PERMISSIONS_LOCATION_WHEN_IN_USE = @"whenInUse";
+
+// Error codes
+NSString* E_SEND_FEEDBACK_ERROR = @"E_SEND_FEEDBACK_ERROR";
+NSString* E_PROFILE_ID_GET_ERROR = @"E_PROFILE_ID_GET_ERROR";
+NSString* E_PROFILE_ID_RESET_ERROR = @"E_PROFILE_ID_RESET_ERROR";
+NSString* E_PROFILE_GET_USER_DATA_ERROR = @"E_PROFILE_GET_USER_DATA_ERROR";
+NSString* E_COUPONS_PARSING_ERROR = @"E_COUPONS_PARSING_ERROR";
+NSString* E_COUPONS_RETRIEVAL_ERROR = @"E_COUPONS_RETRIEVAL_ERROR";
+NSString* E_NOTIFICATION_HISTORY_RETRIEVAL_ERROR = @"E_NOTIFICATION_HISTORY_RETRIEVAL_ERROR";
+NSString* E_OPT_OUT_ERROR = @"E_OPT_OUT_ERROR";
 
 @end
